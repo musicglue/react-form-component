d409a826cc17effb48993f39d6e7f4226ef46ae7
Bjorn Hougaard <bjornzeiler@gmail.com>
2016-04-11 18:43:55 +0100

    formContext is a function

--- src/fields/index.js
+++ src/fields/index.js
@@ -1,5 +1,5 @@
 export * from './inputs';
 export { default as SelectField } from './SelectField';
 export { default as TextareaField } from './TextareaField';
-export { default as FormContext } from './formContext';
 export { default as Field } from './field';
+export { default as formContext } from './formContext';
